4f33334b23f7fe344afbfc22204e61432347bbc5
U-VAIO\apple <vinyel422@gmail.com>
2017-10-29 03:17:31 +0900

    selectMenu

---
+++ Assets/Scripts/SelectAES/TouchObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchObject : MonoBehaviour {
+    // rayが届く範囲
+    public float distance = 100f;
+    void Update() {
+        // 左クリックを取得
+        if (Input.GetMouseButtonDown(0)) {
+            // クリックしたスクリーン座標をrayに変換
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // Rayの当たったオブジェクトの情報を格納する
+            RaycastHit hit = new RaycastHit();
+            // オブジェクトにrayが当たった時
+            if (Physics.Raycast(ray, out hit, distance)) {
+                // rayが当たったオブジェクトの名前を取得
+                string objectName = hit.collider.gameObject.name;
+                Debug.Log(objectName);
+            }
+        }
+    }
+}
